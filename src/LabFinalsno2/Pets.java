7f19bc65aa747303d24ee20de3464a7ede8815f4
RayonB <143682851+RayonB@users.noreply.github.com>
2023-12-11 14:15:32 +0800

    Update Pets.java

---
+++ src/LabFinalsno2/Pets.java
@@ -0,0 +1,12 @@
+package LabFinalsno2;
+
+abstract class Pets {
+    public abstract void Color();
+    
+    public void Dog(){
+        System.out.print("[Buddy ");
+    }
+    public void Cat(){
+        System.out.print("{Night-Night ");
+    }
+}
