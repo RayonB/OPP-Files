43ea3a3c224d951a72cc61e1969d4eba01b86147
RayonB <rinevarayon@gmail.com>
2023-12-11 20:58:16 +0800

    Inheritance

---
+++ src/Inheritance/Main.java
@@ -0,0 +1,14 @@
+package Inheritance;
+
+public class Main {
+    
+    public static void main(String[] args){
+        
+        Child myChild = new Child ("Evarine Rayon");
+        
+        myChild.eat();
+        myChild.sleep();
+        myChild.dance();
+        myChild.clean();
+    }
+}
